6022ba37d363c9438186a740df62761034d7b738
abdvlxamid <emilioiesco@gmail.com>
2025-05-04 15:56:35 +0500

    polimorfizm

---
+++ src/MathFunctions.java
@@ -0,0 +1,22 @@
+public class MathFunctions {
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public void sub(int a, int b){
+        System.out.println(a-b);
+    }
+    public void multiply(int a, int b){
+        System.out.println(a*b);
+    }
+    public void div(int a, int b){
+        System.out.println(a/b);
+    }
+    public void abs(int a){
+
+        System.out.println(a * -1);
+    }
+    public void pow(int a){
+        System.out.println(a*a);
+    }
+
+}
